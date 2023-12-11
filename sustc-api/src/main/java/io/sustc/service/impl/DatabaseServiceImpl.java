8db342e4026a0faffa5397172f4b940c79b82d06
YU Siyao <3167322194@qq.com>
2023-12-11 21:30:34 +0800

    DatabaseServiceImpl finished, haven't tested.

--- sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
+++ sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
@@ -143,42 +143,270 @@ public class DatabaseServiceImpl implements DatabaseService {
         }
 
 
-//        String sql5 = "insert into video_info values(?,?,?,?,?,?,?,?,?)";
-//        try {
-//            PreparedStatement stmt = conn.prepareStatement(sql5);
-//            long cnt = 0;
-//            for (VideoRecord video : videoRecords) {
-//                stmt.setString(1, video.getBv());
-//                stmt.setString(2, video.getTitle());
-//                stmt.setFloat(3, video.getDuration());
-//                stmt.setString(4, video.getDescription());
-//                stmt.setLong(5, video.getOwnerMid());
-//                stmt.setLong(6, video.getReviewer());
-//                stmt.setTimestamp(7, video.getCommitTime());
-//                stmt.setTimestamp(8, video.getReviewTime());
-//                stmt.setTimestamp(9, video.getPublicTime());
-//                stmt.addBatch();
-//                cnt++;
-//                if (cnt % BATCH_SIZE == 0) {
-//                    stmt.executeBatch();
-//                    stmt.clearBatch();
-//                }
-//            }
-//            if (cnt % BATCH_SIZE != 0) {
-//                stmt.executeBatch();
-//                stmt.clearBatch();
-//            }
-//            conn.commit();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            try {
-//                conn.rollback();
-//            } catch (Exception e2) {
-//                e2.printStackTrace();
-//            }
-//        }
+        long maxBv = 0;
+        String sql5 = "insert into video_info values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        String sql6 = "insert into max_bv values(?, ?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql5);
+             PreparedStatement stmt2 = conn.prepareStatement(sql6)) {
+            long cnt = 0;
+            for (VideoRecord video : videoRecords) {
+                maxBv = Math.max(maxBv, Long.parseLong(video.getBv().substring(2)));
+                stmt.setString(1, video.getBv());
+                stmt.setString(2, video.getTitle());
+                stmt.setFloat(3, video.getDuration());
+                stmt.setString(4, video.getDescription());
+                stmt.setLong(5, video.getOwnerMid());
+                stmt.setLong(6, video.getReviewer());
+                stmt.setTimestamp(7, video.getCommitTime());
+                stmt.setTimestamp(8, video.getReviewTime());
+                stmt.setTimestamp(9, video.getPublicTime());
+                stmt.setInt(10, video.getLike().length);
+                stmt.setInt(11, video.getCoin().length);
+                stmt.setInt(12, video.getFavorite().length);
+                stmt.setInt(13, 0);
+                stmt.setInt(14, 0);
+                stmt.setBoolean(15, video.getReviewer() != null);
+                stmt.addBatch();
+                cnt++;
+                if (cnt % BATCH_SIZE == 0) {
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                }
+            }
+            if (cnt % BATCH_SIZE != 0) {
+                stmt.executeBatch();
+                stmt.clearBatch();
+            }
+            if (maxBv != 0) {
+                stmt2.setString(1, "BV" + maxBv);
+                stmt2.setLong(2, maxBv);
+                stmt2.addBatch();
+                stmt2.executeBatch();
+                stmt2.clearBatch();
+                stmt2.close();
+            }
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        String sql7 = "insert into like_video values(?,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql7)) {
+            long cnt = 0;
+            for (VideoRecord video : videoRecords) {
+                long[] follow = video.getLike();
+                String bv = video.getBv();
+                for (int i = 0; i < follow.length; i++) {
+                    stmt.setLong(1, follow[i]);
+                    stmt.setString(2, bv);
+                    stmt.addBatch();
+                    cnt++;
+                    if (cnt % BATCH_SIZE == 0) {
+                        stmt.executeBatch();
+                        stmt.clearBatch();
+                    }
+                }
+                if (cnt % BATCH_SIZE == 0) {
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                }
+            }
+            if (cnt % BATCH_SIZE != 0) {
+                stmt.executeBatch();
+                stmt.clearBatch();
+            }
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        String sql8 = "insert into coin_video values(?,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql8)) {
+            long cnt = 0;
+            for (VideoRecord video : videoRecords) {
+                long[] follow = video.getCoin();
+                String bv = video.getBv();
+                for (int i = 0; i < follow.length; i++) {
+                    stmt.setLong(1, follow[i]);
+                    stmt.setString(2, bv);
+                    stmt.addBatch();
+                    cnt++;
+                    if (cnt % BATCH_SIZE == 0) {
+                        stmt.executeBatch();
+                        stmt.clearBatch();
+                    }
+                }
+                if (cnt % BATCH_SIZE == 0) {
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                }
+            }
+            if (cnt % BATCH_SIZE != 0) {
+                stmt.executeBatch();
+                stmt.clearBatch();
+            }
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
+        String sql9 = "insert into fav_video values(?,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql9)) {
+            long cnt = 0;
+            for (VideoRecord video : videoRecords) {
+                long[] follow = video.getFavorite();
+                String bv = video.getBv();
+                for (int i = 0; i < follow.length; i++) {
+                    stmt.setLong(1, follow[i]);
+                    stmt.setString(2, bv);
+                    stmt.addBatch();
+                    cnt++;
+                    if (cnt % BATCH_SIZE == 0) {
+                        stmt.executeBatch();
+                        stmt.clearBatch();
+                    }
+                }
+                if (cnt % BATCH_SIZE == 0) {
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                }
+            }
+            if (cnt % BATCH_SIZE != 0) {
+                stmt.executeBatch();
+                stmt.clearBatch();
+            }
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        String sql10 = "insert into view_video values(?,?,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql10)) {
+            long cnt = 0;
+            for (VideoRecord video : videoRecords) {
+                long[] view = video.getViewerMids();
+                float[] viewTime = video.getViewTime();
+                String bv = video.getBv();
+                for (int i = 0; i < view.length; i++) {
+                    stmt.setLong(1, view[i]);
+                    stmt.setString(2, bv);
+                    stmt.setFloat(3, viewTime[i]);
+                    stmt.addBatch();
+                    cnt++;
+                    if (cnt % BATCH_SIZE == 0) {
+                        stmt.executeBatch();
+                        stmt.clearBatch();
+                    }
+                }
+                if (cnt % BATCH_SIZE == 0) {
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                }
+            }
+            if (cnt % BATCH_SIZE != 0) {
+                stmt.executeBatch();
+                stmt.clearBatch();
+            }
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        String sql11 = "insert into danmu_info (bv , mid , time, content , post_time) values ( ?, ?, ?, ? ,? )";
+        try(PreparedStatement stmt = conn.prepareStatement(sql11)){
+            long cnt = 0;
+            for (int i = 0; i < danmuRecords.size(); i++) {
+                DanmuRecord danmu =danmuRecords.get(i);
+                stmt.setString(1,danmu.getBv());
+                stmt.setLong(2,danmu.getMid());
+                stmt.setFloat(3,danmu.getTime());
+                stmt.setString(4,danmu.getContent());
+                stmt.setTimestamp(5,danmu.getPostTime());
+                cnt++;
+                stmt.addBatch();
+                if (cnt % BATCH_SIZE == 0) {
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                }
+            }
+            if (cnt % BATCH_SIZE != 0) {
+                stmt.executeBatch();
+                stmt.clearBatch();
+            }
+            conn.commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
 
 
+        String sql12 = "insert into like_danmu values(?,?)";
+        try(PreparedStatement stmt = conn.prepareStatement(sql12) ) {
+            long cnt = 0;
+            for (int i = 0; i < danmuRecords.size(); i++) {
+                DanmuRecord danmu = danmuRecords.get(i);
+                long[] follow = danmu.getLikedBy();
+                for (int j = 0; j < follow.length; j++) {
+                    stmt.setLong(1, follow[j]);
+                    stmt.setInt(2, i+1);
+                    stmt.addBatch();
+                    cnt++;
+                    if (cnt % BATCH_SIZE == 0) {
+                        stmt.executeBatch();
+                        stmt.clearBatch();
+                    }
+                }
+                if (cnt % BATCH_SIZE == 0) {
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                }
+            }
+            if (cnt % BATCH_SIZE != 0) {
+                stmt.executeBatch();
+                stmt.clearBatch();
+            }
+            conn.commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+            try {
+                conn.rollback();
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
+        try {
+            conn.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     /*
@@ -193,7 +421,7 @@ public class DatabaseServiceImpl implements DatabaseService {
         // You can use the default truncate script provided by us in most cases,
         // but if it doesn't work properly, you may need to modify it.
         String[] tableName = {"like_danmu", "view_video", "like_video", "fav_video", "coin_video", "max_bv",
-                 "danmu_info", "video_info", "follow", "max_mid", "user_auth", "user_info"};
+                "danmu_info", "video_info", "follow", "max_mid", "user_auth", "user_info"};
         try (Connection conn = dataSource.getConnection();
              Statement stmt = conn.createStatement()) {
             for (int i = 0; i < tableName.length; i++) {
